359e38c34a52c7e426c1545f1c5d8d95f238295f
Jared Ma <jaredma1234@gmail.com>
2025-02-01 22:27:16 -0800

    adjust ui layout

--- terminal_tetris.c
+++ terminal_tetris.c
@@ -9,30 +9,30 @@ int main(int argc, char* argv[argc+1]) {
     noecho();  // Don't echo input to screen
     refresh();
 
-    int hold_window_h = 7;
+    int hold_window_h = 6;
     int hold_window_w = 14;
     int hold_window_y = 0;
     int hold_window_x = 0;
     WINDOW* hold_window = newwin(hold_window_h, hold_window_w, hold_window_y, hold_window_x);
     box(hold_window, 0, 0);
-    mvwprintw(hold_window, 1, 1, "HOLD");
+    mvwprintw(hold_window, 0, 1, "HOLD");
+    mvwprintw(hold_window, 2, 5, "[][]");
     mvwprintw(hold_window, 3, 5, "[][]");
-    mvwprintw(hold_window, 4, 5, "[][]");
     wrefresh(hold_window);
 
-    int stats_window_h = 15;
+    int stats_window_h = 16;
     int stats_window_w = 14;
     int stats_window_y = hold_window_h;
     int stats_window_x = 0;
     WINDOW* stats_window = newwin(stats_window_h, stats_window_w, stats_window_y, stats_window_x);
     box(stats_window, 0, 0);
-    mvwprintw(stats_window, 1, 1, "STATS");
-    mvwprintw(stats_window, 3, 1, "time:");
-    mvwprintw(stats_window, 5, 1, "score:");
-    mvwprintw(stats_window, 7, 1, "lines:");
-    mvwprintw(stats_window, 9, 1, "level:");
-    mvwprintw(stats_window, 11, 1, "combo:");
-    mvwprintw(stats_window, 13, 1, "fps:");
+    mvwprintw(stats_window, 0, 1, "STATS");
+    mvwprintw(stats_window, 2, 1, "time:");
+    mvwprintw(stats_window, 4, 1, "score:");
+    mvwprintw(stats_window, 6, 1, "lines:");
+    mvwprintw(stats_window, 8, 1, "level:");
+    mvwprintw(stats_window, 10, 1, "combo:");
+    mvwprintw(stats_window, 12, 1, "fps:");
     wrefresh(stats_window);
 
     int play_window_h = 22;
@@ -46,30 +46,30 @@ int main(int argc, char* argv[argc+1]) {
     }
     wrefresh(play_window);
 
-    int next_window_h = 7;
+    int next_window_h = 6;
     int next_window_w = 14;
     int next_window_y = 0;
     int next_window_x = play_window_x + play_window_w;
     WINDOW* next_window = newwin(next_window_h, next_window_w, next_window_y, next_window_x);
     box(next_window, 0, 0);
-    mvwprintw(next_window, 1, 1, "NEXT");
+    mvwprintw(next_window, 0, 1, "NEXT");
+    mvwprintw(next_window, 2, 5, "[][]");
     mvwprintw(next_window, 3, 5, "[][]");
-    mvwprintw(next_window, 4, 5, "[][]");
     wrefresh(next_window);
 
-    int controls_window_h = 15;
+    int controls_window_h = 16;
     int controls_window_w = 14;
     int controls_window_y = next_window_h;
     int controls_window_x = next_window_x;
     WINDOW* controls_window = newwin(controls_window_h, controls_window_w, controls_window_y, controls_window_x);
     box(controls_window, 0, 0);
-    mvwprintw(controls_window, 1, 1, "CONTROLS");
-    mvwprintw(controls_window, 3, 1, "move:    < >");
-    mvwprintw(controls_window, 5, 1, "rotate:  z x");
-    mvwprintw(controls_window, 7, 1, "hold:  shift");
-    mvwprintw(controls_window, 9, 1, "soft drop: v");
-    mvwprintw(controls_window, 11, 1, "hard drop: _");
-    mvwprintw(controls_window, 13, 1, "pause:   esc");
+    mvwprintw(controls_window, 0, 1, "CONTROLS");
+    mvwprintw(controls_window, 2, 1, "move:    < >");
+    mvwprintw(controls_window, 4, 1, "rotate:  z x");
+    mvwprintw(controls_window, 6, 1, "hold:  shift");
+    mvwprintw(controls_window, 8, 1, "soft drop: v");
+    mvwprintw(controls_window, 10, 1, "hard drop: _");
+    mvwprintw(controls_window, 12, 1, "pause:   esc");
     wrefresh(controls_window);
     
     getch();
