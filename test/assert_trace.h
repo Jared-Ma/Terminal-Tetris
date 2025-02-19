bb1e535a571fc90ba128f0d359249744c05067a6
Jared Ma <jaredma1234@gmail.com>
2025-02-18 21:55:35 -0800

    implement assertion macro that prints trace

---
+++ test/assert_trace.h
@@ -0,0 +1,18 @@
+#ifndef TRACE_ASSERT_H
+#define TRACE_ASSERT_H
+#include <stdbool.h>
+#include <assert.h>
+
+#define ASSERT(condition)                                  \
+    if (!(condition)) {                                    \
+        printf(                                            \
+            "%s:%d: %s: Assertion (%s) failed.\n",         \
+            __FILE__,                                      \
+            __LINE__,                                      \
+            __func__,                                      \
+            #condition                                     \
+        );                                                 \
+        return false;                                      \
+    }                                                      \
+
+#endif
