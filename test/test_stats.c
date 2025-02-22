2a71f587f785c1d99a79fa1b77fa5bc9b8ca2055
Jared Ma <jaredma1234@gmail.com>
2025-02-22 01:39:53 -0800

    remove precondition assertion in test_stats_reset

--- test/test_stats.c
+++ test/test_stats.c
@@ -25,8 +25,6 @@ bool test_stats_reset(void) {
     Stats stats = stats_get();
     stats.time = 1;
     stats.frame_count = 1;
-    ASSERT(stats.time == 1);
-    ASSERT(stats.time == 1);
     stats_reset(&stats);
     ASSERT(stats.time == 0);
     ASSERT(stats.frame_count == 0);
