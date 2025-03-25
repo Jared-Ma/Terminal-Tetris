25dab4895d1dfd7771c147ccedcb8e9a16f64459
Jared Ma <jaredma1234@gmail.com>
2025-03-25 15:21:50 -0700

    add debug log filepath constant

--- src/logger.c
+++ src/logger.c
@@ -4,12 +4,14 @@
 #include <stdio.h>
 
 
+const char* DEBUG_LOG_FILEPATH =  "./logs/debug.txt";
+
 FILE* debug_log = NULL;
 
-bool debug_log_open(const char* filename) {
-    debug_log = fopen(filename, "w");
+bool debug_log_open(const char* filepath) {
+    debug_log = fopen(filepath, "w");
     if (!debug_log) {
-        fprintf(stderr, "Failed to open file %s for writing.", filename);
+        fprintf(stderr, "Failed to open file (%s) for writing.", filepath);
         return false;
     }
     return true;
--- src/logger.h
+++ src/logger.h
@@ -4,8 +4,10 @@
 #include <stdio.h>
 
 
+extern const char* DEBUG_LOG_FILEPATH;
+
 extern FILE* debug_log;
 
-bool debug_log_open(const char* filename);
+bool debug_log_open(const char* filepath);
 
 #endif
