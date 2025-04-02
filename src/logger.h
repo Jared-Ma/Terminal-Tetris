33d2d3eba8f38405d5109f408506b8874f98ba12
Jared Ma <jaredma1234@gmail.com>
2025-04-01 20:19:10 -0700

    implement debug trace logging

--- src/logger.h
+++ src/logger.h
@@ -3,6 +3,54 @@
 #include <stdbool.h>
 #include <stdio.h>
 
+/**
+ ** @brief Converts a macro to a string
+ **/
+#define MACRO_TO_STRING(X) TO_STRING(X)
+#define TO_STRING(X) #X
+
+/**
+ ** @brief Extracts the first argument from a list of arguments
+ **/
+#define ARGS_HEAD(...) ARGS_HEAD0(__VA_ARGS__, 0)
+#define ARGS_HEAD0(_0, ...) _0
+
+/**
+ ** @brief Removes the first argument from a list of arguments
+ **/
+#define ARGS_TAIL(...) ARGS_TAIL0(__VA_ARGS__, 0)
+#define ARGS_TAIL0(_0, ...) __VA_ARGS__
+
+/**
+ ** @brief Trace prints to debug log
+ **
+ ** This implementation has the particularity of adding a format @c "%.0d" to 
+ ** skip the last element of the list, which was artificially added.
+ **/
+#define TRACE_PRINT(...)                  \
+TRACE_PRINT_AUX(                          \
+    ARGS_HEAD(__VA_ARGS__) "%.0d",        \
+    ARGS_TAIL(__VA_ARGS__)                \
+)
+
+/**
+ ** @brief Allows multiple arguments to be printed in the same trace
+ **/
+#define TRACE_PRINT_AUX(F, ...)                    \
+do {                                               \
+    if (debug_mode)                                \
+        fprintf(                                   \
+            debug_log,                             \
+            "[%s %s] - %s:%s:%s - " F "\n",        \
+            __DATE__,                              \
+            __TIME__,                              \
+            __FILE__,                              \
+            __func__,                              \
+            MACRO_TO_STRING(__LINE__),             \
+            __VA_ARGS__                            \
+        );                                         \
+} while (false)
+
 
 extern const char* DEBUG_LOG_FILEPATH;
 
